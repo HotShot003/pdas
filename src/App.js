78b0a2b2714e8fe2ec8d32125f078d0a2ee39941
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-06-06 00:51:29 +0530

    feat: Create ProfileCard Component and Implement Bulma CSS
    - Started learning React by creating a ProfileCard component.
    - Implemented Bulma CSS for styling.
    - Created a main App component to display ProfileCards for Alexa, Cortana, and Siri.
    - Added images and descriptions for each digital assistant.
    - Structured the layout using Bulma's grid system.
    
    This commit marks the initial setup and basic functionality of the project.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./Images/alexa.png";
+import CortanaImage from "./Images/cortana.png";
+import SiriImage from "./Images/siri.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistant</p>
+        </div>
+      </section>
+      {/* <div>Personal Digital Assistant</div> */}
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa"
+                image={AlexaImage}
+                description="Alexa was create by Amazon and help u buy things"
+              />
+            </div>
+
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana"
+                image={CortanaImage}
+                description="Cortana was made by Microsoft"
+              />
+            </div>
+
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri"
+                image={SiriImage}
+                description="Siri was made by Apple"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+      {/* <img src={AlexaImage} alt=""/>  */}
+      {/* <img src={CortanaImage}alt=""/> */}
+      {/* <img src={SiriImage} alt=""/> */}
     </div>
   );
 }
-
 export default App;
